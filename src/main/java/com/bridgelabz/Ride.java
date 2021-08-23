e13e87c97f0d657d20f07ce402532d93404d8cd6
omipapal12 <omipapal12@gmail.com>
2021-08-23 20:49:46 +0530

    Added: Bonus

--- src/main/java/com/bridgelabz/Ride.java
+++ src/main/java/com/bridgelabz/Ride.java
@@ -3,9 +3,11 @@ package com.bridgelabz;
 public class Ride {
     public double distance;
     public int time;
+    public int minute;
 
-    public Ride(double distance, int time) {
+    public Ride(double distance, int time, int minute) {
         this.distance = distance;
         this.time = time;
+        this.minute = minute;
     }
 }
