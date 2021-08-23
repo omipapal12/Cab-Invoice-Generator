fa398552cbbb67c4d571d5ad0f05db0a0ea544de
omipapal12 <omipapal12@gmail.com>
2021-08-23 19:58:44 +0530

    Added: Enhanced Invoice

--- src/test/java/com/bridgelabz/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/InvoiceServiceTest.java
@@ -5,49 +5,36 @@ import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 public class InvoiceServiceTest {
-    InvoiceGenerator invoiceGenerator = null;
-    /*
-     * Purpose : To create object before any test case is called.
-     * Improves Redundancy of code
-     */
-    @Before
-    public void setUp() {
-        invoiceGenerator = new InvoiceGenerator();
-    }
-    /*
-     * Step 1.1
-     * Purpose : Given distance and time,
-     * return total fare
-     */
+    InvoiceGenerator cabInvoiceGenerator = new InvoiceGenerator();
+
     @Test
-    public void givenDistanceAndTime_ShouldReturnTotalFare() {
-        double distance = 2.0;
-        int time = 5;
-        double fare = invoiceGenerator.calculateFare(distance, time);
-        Assert.assertEquals(25, fare, 0.0);
+    public void givenDistanceAndTime_WhenCalculated_ReturnTotalFare()
+    {
+        double cabInvoiceGeneratorTotalFare = cabInvoiceGenerator.getTotalFare(5.0, 5);
+        Assert.assertEquals(55.0, cabInvoiceGeneratorTotalFare, 0);
     }
-    /*
-     * Step 1.2
-     * Purpose: Given distance and time,
-     * return minimum fare as 5
-     */
+
     @Test
-    public void givenLessDistanceAndTime_ShouldReturnMinimumFare() {
-        double distance = 0.1;
-        int time = 1;
-        double fare = invoiceGenerator.calculateFare(distance, time);
-        Assert.assertEquals(5, fare, 0.0);
+    public void givenDistanceAndTime_WhenLessThanMinimumFareGetCalculated_ThenReturnMinimumFare()
+    {
+        double cabInvoiceGeneratorTotalFare = cabInvoiceGenerator.getTotalFare(0.132, 1);
+        Assert.assertEquals(5, cabInvoiceGeneratorTotalFare, 0);
     }
-    /*
-     * Step 2
-     * Purpose : Given distance and time for multiple rides,
-     * return the aggregate total for all the rides
-     */
+
     @Test
-    public void givenMultipleRides_ShouldReturnTotalFare() {
-        Ride[] rides = {new Ride(2.0, 5),
-                new Ride(0.1, 1)};
-        double fare = invoiceGenerator.calculateTotalFare(rides);
-        Assert.assertEquals(30, fare, 0.0);
+    public void givenMultipleRides_WhenCalculated_ReturnTotalFare()
+    {
+        Ride[] rides = {new Ride(26.05, 29), new Ride(12.39, 25)};
+        double cabInvoiceGeneratorTotalFare = cabInvoiceGenerator.getTotalFare(rides);
+        Assert.assertEquals(438.4, cabInvoiceGeneratorTotalFare, 0);
+    }
+
+    @Test
+    public void givenMultipleRides_WhenCalculated_ReturnInvoiceSummery()
+    {
+        Ride[] rides = {new Ride(25.12, 40), new Ride(12.39, 25)};
+        InvoiceSummary invoiceSummary = cabInvoiceGenerator.getInvoiceSummary(rides);
+        InvoiceSummary summary = new InvoiceSummary(2, 440.1);
+        Assert.assertEquals(summary, invoiceSummary);
     }
 }
